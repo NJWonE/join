8d3c11fd790503e7195d9b284956a4af65aeede6
NJWonE <nohjaewon123@daum.net>
2021-10-24 10:48:08 +0900

    연습중

--- app/app.js
+++ app/app.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src`));
 
 const home = require("./src/routes/index");
 app.use("/", home);
